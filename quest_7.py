e1d8d5a2a89af6852e02cebcc9532ccdd9de95b3
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-29 16:30:00 +0000

    quest_7

---
+++ quest_7.py
@@ -0,0 +1,6 @@
+# Escreva um programa que solicite ao usuário seu nome e idade e imprima uma mensagem formatada no seguinte 
+# formato: "Olá, {nome}! Você tem {idade} anos."
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+print(f"Olá, {nome}! Você tem {idade} anos.")
