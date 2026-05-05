ce5f054658a69b611276848fdb45984b97a4d761
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-05-05 16:51:34 +0000

    quest_14.py

---
+++ quest_14.py
@@ -0,0 +1,8 @@
+# 14) Escreva um programa que solicite ao usuário uma palavra e imprima uma mensagem formatada mostrando a palavra em caixa alta e 
+# caixa baixa.
+
+palavra = input("Digite uma palavra: ")
+
+print("Caixa alta:", palavra.upper())
+print("Caixa baixa:", palavra.lower())
+
