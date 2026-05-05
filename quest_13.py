1b514db72af33cf582f2108c187ab72114e6ef81
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-05-05 16:47:45 +0000

    quest_13

---
+++ quest_13.py
@@ -0,0 +1,5 @@
+# 13) Escreva um programa que solicite ao usuário um número decimal e imprima uma mensagem formatada mostrando o número com duas 
+# casas decimais.
+
+numero = float(input("Digite um número decimal: "))
+print(f"Número formatado: {numero:.2f}")
