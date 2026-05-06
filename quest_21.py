2c7c7b76112dcda9dcf11ce24301fd660252197f
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-05-06 16:37:03 +0000

    quest_21

---
+++ quest_21.py
@@ -0,0 +1,5 @@
+# 21) Escreva um programa que solicite ao usuário para digitar seu nome e imprima-o em formato invertido. 
+# Por exemplo, se o nome for "João Silva", o programa deve imprimir "avliS oãoJ".
+
+nome = input("Digite seu nome: ")
+print(nome[::-1])
