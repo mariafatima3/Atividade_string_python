f20c0b0ab503ba991669f31624444915554404e0
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-05-06 16:31:14 +0000

    quest_20

---
+++ quest_20.py
@@ -0,0 +1,7 @@
+# 20) Escreva um programa que solicite ao usuário para digitar uma frase e substitua todas as ocorrências da letra "a" por "@".
+
+frase = input("Digite uma frase: ")
+
+frase = frase.replace("a", "@")
+
+print(f"Frase formatada: {frase}")
