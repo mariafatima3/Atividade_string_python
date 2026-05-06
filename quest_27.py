b947d7c92c5b5f25fb78c3c17c0dd422cac56c8f
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-05-06 18:35:51 +0000

    quest_27

---
+++ quest_27.py
@@ -0,0 +1,6 @@
+# 27) Escreva um programa que solicite ao usuário para digitar seu nome em letras minúsculas e, em seguida, imprima o nome com a primeira letra em maiúscula.
+
+nome = input("Digite seu nome em letras minúsculas: ")
+letras = nome.capitalize()
+
+print("Seu nome formatado é:", letras)
