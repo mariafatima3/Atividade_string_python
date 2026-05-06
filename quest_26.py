0955e7ba22bdcddf2a6bf23919494a0a552d8771
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-05-06 18:30:08 +0000

    quest_26

---
+++ quest_26.py
@@ -0,0 +1,6 @@
+# 26) Escreva um programa que peça ao usuário para digitar uma frase e imprima o número de caracteres na frase.
+
+frase = input("Digite uma frase: ")
+numero_de_caracteres = len(frase)
+
+print("O número de caracteres na frase é:", numero_de_caracteres)
